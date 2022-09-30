0474c0a43901e46725ef34d055ee1534fb629c59
jswork <jonathanjshin@gmail.com>
2022-09-30 12:57:28 -0700

    Set up kebab to camel function

---
+++ in-class-exercises/1-intro-and-review/kebabToCamelCase.js
@@ -0,0 +1,16 @@
+// Convert kebab case words into camel casing. The first word within the output should be capitalized only if it is the first letter of a word and dashes should be removed.
+
+// * change-this-word gets converted into changeThisWord
+// *day-month-year gets converted into dayMonthYear
+// ALL-CAPS gets converted ubti allCaps
+
+function kebabToCamel (kebabString) {
+    let camelString
+    
+    //loop over kebabstring 
+    //- if the char dash, don't use it AND the following char would be capital
+    // - otherwise, copy lowercased to camelString
+
+    //test-string => testString
+    return camelString;
+}
